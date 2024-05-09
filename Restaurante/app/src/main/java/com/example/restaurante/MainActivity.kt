cf61a22aa3b42ec58cfc62cb9a9f2d5b4bd5f246
miguelvianab <mvbmengao@gmail.com>
2024-05-09 14:17:02 -0300

    MainActivity.kt

--- Restaurante/app/src/main/java/com/example/restaurante/MainActivity.kt
+++ Restaurante/app/src/main/java/com/example/restaurante/MainActivity.kt
@@ -7,55 +7,55 @@ import com.example.restaurante.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding:ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        binding = ActivityMainBinding.inflate(layoutInflater)
-
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-
+        // Obtém o nome de usuário passado como extra da LoginActivity e exibe uma mensagem de boas-vindas na tela.
         val username = intent.extras?.getString("username")
-
-        if(!username.isNullOrEmpty()){
-            binding.textOla.setText("Olá $username")
+        if (!username.isNullOrEmpty()) {
+            binding.textOla.text = "Olá $username"
         }
 
+        // Configura um listener para o botão "Fechar" que encerra a aplicação.
         binding.buttonFechar.setOnClickListener {
             finishAffinity()
         }
 
-
+        // Configura um listener para o checkbox "Pizza" que exibe a quantidade e o preço da pizza selecionada.
         binding.checkPizza.setOnClickListener {
-            if(binding.checkPizza.isChecked()){
+            if (binding.checkPizza.isChecked()) {
                 binding.editQuantidadePizza.setText("1")
                 binding.textPrecoPizza.visibility = View.VISIBLE
-            }else{
+            } else {
                 binding.editQuantidadePizza.setText("0")
                 binding.textPrecoPizza.visibility = View.GONE
             }
         }
 
+        // Configura um listener para o checkbox "Salada" que exibe a quantidade e o preço da salada selecionada.
         binding.checkSalada.setOnClickListener {
-            if(binding.checkSalada.isChecked()){
+            if (binding.checkSalada.isChecked()) {
                 binding.editQuantidadeSalada.setText("1")
                 binding.textPrecoSalada.visibility = View.VISIBLE
-            }else{
+            } else {
                 binding.editQuantidadeSalada.setText("0")
                 binding.textPrecoSalada.visibility = View.GONE
             }
         }
 
+        // Configura um listener para o checkbox "Hamburger" que exibe a quantidade e o preço do hambúrguer selecionado.
         binding.checkHamburger.setOnClickListener {
-            if(binding.checkHamburger.isChecked()){
+            if (binding.checkHamburger.isChecked()) {
                 binding.editQuantidadeHamburguer.setText("1")
                 binding.textPrecoHamburguer.visibility = View.VISIBLE
-            }else{
+            } else {
                 binding.editQuantidadeHamburguer.setText("0")
                 binding.textPrecoHamburguer.visibility = View.GONE
             }
         }
-
     }
 }
