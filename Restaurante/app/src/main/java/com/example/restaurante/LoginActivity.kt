a28f31a41e16f8b36c5c6ea5e91b72c6202d3891
miguelvianab <mvbmengao@gmail.com>
2024-05-09 14:14:12 -0300

    LoginActivity.kt

--- Restaurante/app/src/main/java/com/example/restaurante/LoginActivity.kt
+++ Restaurante/app/src/main/java/com/example/restaurante/LoginActivity.kt
@@ -8,26 +8,30 @@ import com.example.restaurante.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
 
-    private lateinit var binding:ActivityLoginBinding
+    private lateinit var binding: ActivityLoginBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        // Configura um listener para o botão de login
         binding.buttonEntrar.setOnClickListener {
+            // Obtém o nome de usuário e a senha inseridos nos campos de texto
             val username = binding.editUsername.text.toString().trim()
             val password = binding.editPassword.text.toString().trim()
 
-            if (username.equals("abc") && password.equals("123")) {
+            // Verifica se o nome de usuário e a senha estão corretos
+            if (username == "abc" && password == "123") {
+                // Se estiverem corretos, inicia a MainActivity passando o nome de usuário como um extra
                 val i = Intent(this, MainActivity::class.java)
                 i.putExtra("username", username)
                 startActivity(i)
                 finish()
             } else {
+                // Caso contrário, exibe uma mensagem de erro usando Toast
                 Toast.makeText(applicationContext, "Errou", Toast.LENGTH_LONG).show()
             }
         }
-
     }
 }
